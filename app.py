e0c4402358754a1324f4da711dfb43c9d39fc091
selvaram ganesh <gsriram7@gmail.com>
2018-04-01 07:05:10 -0700

    Added Twilio integration Co-authored-by: InvalidCS <cdipalma@uci.edu>

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import sentiment
+from twilio.rest import Client
 
 app = Flask(__name__)
+
+account = 'ACc89d674cfce611bdb7ec2482511428d8'
+token = 'c9a1c5ef2ff95d8eed2f82a55fef9681'
+client = Client(account, token)
 
 @app.route('/')
 def index():
@@ -15,6 +20,13 @@ def senti():
     print(text)
     avg_sentiment = sentiment.get_average_sentiment(text)
     return jsonify(avg_sentiment)
+
+@app.route('/sms', methods=["POST"])
+def sms():
+    text = request.data
+    message = client.messages.create(to="+12132040146", from_="+12134087788",
+                                     body=text)
+    return text
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
